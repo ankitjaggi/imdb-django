e3e2ad4d50673a42f29cf64c54947dfd71b42952
Ankit Jaggi <ankit.jaggi@live.in>
2015-06-22 14:57:48 +0530

    Bug fixes and UI improvements

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = '4-rt1=37!awv9o993+-z59^xkziv-9%*!pyvh-nc5tvt20d!td'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = '*'
 
 
@@ -101,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
